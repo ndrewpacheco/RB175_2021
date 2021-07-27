632872fb69e6554cbed0f96a4a09966799ca807c
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-07-27 11:22:01 -0400

    added book viewer

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  @contents = File.readlines('data/toc.txt')
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines('data/toc.txt')
+  @chapter = File.read('data/chp1.txt')
+
+  erb :chapter
+
+end
+
+
